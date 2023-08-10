1081b8578e3fa105d75b62a9092c06ce9a15c257
Jose Albornoz <prueba@outlook.cl>
2023-08-09 23:10:52 -0400

    Se modifica el componente Registro y SocialButton para que atraves de props se creen los botones

--- src/componentes/Registro.jsx
+++ src/componentes/Registro.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import SocialButton from './SocialButton'
+import React from 'react';
+import SocialButton from './SocialButton';
 
 export default function Registro() {
   return (
     <>
-    <h1 className='text-dark mb-3'>Crea una cuenta</h1>
-    <SocialButton />
-    <p className='text-dark mt-3'>O usa tu email para registrarte.</p>
+      <h1 className="text-dark mb-3">Crea una cuenta</h1>
+      <SocialButton button_1={'fa-facebook'} button_2={'fa-github'} button_3={'fa-linkedin'} />
+      <p className="text-dark mt-3">O usa tu email para registrarte.</p>
     </>
-  )
+  );
 }
--- src/componentes/SocialButton.jsx
+++ src/componentes/SocialButton.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
-export default function SocialButton() {
+export default function SocialButton({button_1, button_2, button_3}) {
   return (
     <>
-    <Button variant="outline-dark rounded-circle p-2 mx-2"><i className="fa-brands fa-facebook fa-xl"></i></Button>
-    <Button variant="outline-dark rounded-circle p-2 mx-2"><i className="fa-brands fa-github fa-xl"></i></Button>
-    <Button variant="outline-dark rounded-circle p-2 mx-2"><i className="fa-brands fa-linkedin fa-xl"></i></Button>
+      <Button variant="outline-dark rounded-circle p-2 mx-2">
+        <i className={`fa-brands ${button_1} fa-xl`}></i>
+      </Button>
+      <Button variant="outline-dark rounded-circle p-2 mx-2">
+        <i className={`fa-brands ${button_2} fa-xl`}></i>
+      </Button>
+      <Button variant="outline-dark rounded-circle p-2 mx-2">
+        <i className={`fa-brands ${button_3} fa-xl`}></i>
+      </Button>
     </>
-  )
+  );
 }

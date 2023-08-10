25cff1371e87c7bc36aa9c24843e1fc14183dd49
Jose Albornoz <prueba@outlook.cl>
2023-08-10 00:17:58 -0400

    Se crea el componente Formulario y Alert en blanco

--- src/componentes/Registro.jsx
+++ src/componentes/Registro.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import SocialButton from './SocialButton';
+import Formulario from './Formulario';
+import Alert from './Alert';
 
 export default function Registro() {
   return (
@@ -7,6 +9,8 @@ export default function Registro() {
       <h1 className="text-dark mb-3">Crea una cuenta</h1>
       <SocialButton button_1={'fa-facebook'} button_2={'fa-github'} button_3={'fa-linkedin'} />
       <p className="text-dark mt-3">O usa tu email para registrarte.</p>
+      <Formulario />
+      <Alert />
     </>
   );
 }
